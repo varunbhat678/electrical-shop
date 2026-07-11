06d76d48eccde0ba929a3f4b092d7e2c328c1c76
varunbhat678 <bhatvarun37@gmail.com>
2026-07-11 17:38:09 +0530

    Migrated project from SQLite to PostgreSQL

--- electrical_shop/settings.py
+++ electrical_shop/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'electrical_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'electrical_shop',
+        'USER': 'postgres',
+        'PASSWORD': '@Nnm23ri068',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
