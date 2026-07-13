9851835f213c0f2ce1937312c1b28538ef051221
varunbhat678 <bhatvarun37@gmail.com>
2026-07-14 00:02:26 +0530

    Update production settings after PostgreSQL deployment

--- electrical_shop/settings.py
+++ electrical_shop/settings.py
@@ -27,9 +27,11 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "electrical-shop-7pcp.onrender.com",
+]
 
 
 # Application definition
